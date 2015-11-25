264aef0c6eb4a702bb72144daeffc4575e18840e
Marco Lau <marco.lau@industrieit.com>
2015-11-25 17:28:06 +0800

    Add mock backend call for data

--- www/modules/dashboard/dashboard.controller.js
+++ www/modules/dashboard/dashboard.controller.js
@@ -2,9 +2,17 @@
 
 angular.module('dashboard', ['user'])
 
-.controller('DashboardCtrl', function($scope, $location, AuthenticationService) {
+.controller('DashboardCtrl', function($scope, $location, $http, AuthenticationService) {
+  $http.get('json/securities.json')
+    .success(function(data, status, headers, config) {
+      $scope.sData = data.map(JSON.parse)
+    })
+    .error(function(data, status, headers, config) {
+      console.log(status);
+    })
+
   $scope.logout = function() {
     AuthenticationService.Logout();
     $location.path('/users');
   }
-});
+})
